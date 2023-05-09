8e0ff7a3d5bed5c6a58bd0c9645c05fda7440f28
Riya Rajput <78618457+reerajput930@users.noreply.github.com>
2023-05-09 10:32:39 +0530

    cotributor css file removed from index.js

--- client/dashboard/src/index.js
+++ client/dashboard/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import "../src/component/NavBar.css"
-import "../src/component/Contributor.css"
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
