75622a35ec25c183a75ac436ec11f241964e098d
Riya Rajput <78618457+reerajput930@users.noreply.github.com>
2023-05-17 19:25:13 +0530

    deleted ./component/testing

--- client/dashboard/src/App.js
+++ client/dashboard/src/App.js
@@ -13,7 +13,7 @@ import Contributers from "./component/Contributers";
 import Register from "./component/Register";
 import Login from "./component/Login";
 import UserInfo from "./component/UserInfo";
-import Testing from "./component/Testing";
+// import Testing from "./component/Testing";
 import { useAuthContext } from "./hooks/useAuthContext";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -52,7 +52,7 @@ export default function App() {
       )}
       <Routes>
         <Route path="/calender" element={<Calender/>}  />
-        <Route path="/testing" element={<Testing/>}/>
+        {/* <Route path="/testing" element={<Testing/>}/> */}
         <Route path="/userinfo" element={<UserInfo/>}/>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
